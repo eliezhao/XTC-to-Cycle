e2e558eb8f14e8fb3fb5d62a69c481a44196ccc3
C-B-Elite <czgelite@163.com>
2022-07-04 16:57:00 +0800

    update

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-### canister_id
-- to which canister
-- let canister_id = Principal::from_text("YOUR CANISTER ID").expect("wrap principal failed");
-
-### Crt Path
-- the .crt file exported by plug wallet 
-- let crt_path = String::from("CRT FILE PATH");
-
-### cycle amount
-- 1 T = 1e12 
-- let amount = 1e12 as u64;
+### How to use:
+Get Help Info:  
+```shell
+cargo run -- -h
+```
+Use Case:  
+```shell
+cargo run -- -p your_private_key_file_path -c canister_principal -a cycle_amount
+```
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use candid::{Decode, Encode, Principal, CandidType};
-use ic_agent::Agent;
+use candid::{CandidType, Decode, Encode, Principal};
+use clap::{App, Arg};
 use ic_agent::agent::http_transport::ReqwestHttpReplicaV2Transport;
 use ic_agent::identity::Secp256k1Identity;
+use ic_agent::Agent;
 use serde::Deserialize;
 
 #[derive(CandidType, Deserialize, Debug)]
@@ -25,25 +26,65 @@ struct BurnArgs {
 
 #[tokio::main]
 async fn main() {
-    // canister id写给哪个 canister充cycle
-    // @param canister_id : to which canister
-    let canister_id = Principal::from_text("").expect("wrap principal failed");
-    // crt path 写 plug 导出的密钥的绝对路径
-    // @param crt path : the .crt file exported by plug wallet
-    let crt_path = String::from("");
-    // amount : 冲多少cycle，如果 1T 就写 1e12
+    let matches = App::new("XTC to Cycle App")
+        .version("0.1.0")
+        .author("Elie@MixLabs")
+        .about("Help Dev to use XTC")
+        .arg(
+            Arg::with_name("path")
+                .short('p')
+                .long("path")
+                .takes_value(true)
+                .help("private key path"),
+        )
+        .arg(
+            Arg::with_name("canister_id")
+                .short('c')
+                .long("canister")
+                .takes_value(true)
+                .help("top up to the canister specified by canister's principal"),
+        )
+        .arg(
+            Arg::with_name("cycle_amount")
+                .short('a')
+                .long("amount")
+                .takes_value(true)
+                .help("cycle amount, unit : T. If you input 1, the cycle amount will be 1_000_000_000_000"),
+        )
+        .get_matches();
+
+    let canister_id = Principal::from_text(
+        matches
+            .value_of("canister_id")
+            .expect("please specify the canister's principal"),
+    )
+    .expect("wrap principal failed");
+
+    let crt_path = String::from(
+        matches
+            .value_of("path")
+            .expect("please specify the path of your plug crt file or pem file"),
+    );
+
     // @param amount : cycle amount, 1 T = 1e12
-    let amount = 1e12 as u64;
-    xtc2cycle(crt_path, canister_id, amount).await
+    let amount = 1e12
+        * matches
+            .value_of("cycle_amount")
+            .expect("please specify how much xtc should be consumed")
+            .parse::<f64>()
+            .expect("please type in a Integer");
+    println!(
+        "top up Info: \n\tcanister_id : {} \n\tcycle amount: {}",
+        canister_id, amount
+    );
+    xtc2cycle(crt_path, canister_id, amount as u64).await
 }
 
-pub async fn xtc2cycle(crt_path : String, to: Principal, amount : u64) {
+pub async fn xtc2cycle(crt_path: String, to: Principal, amount: u64) {
     let url = "https://ic0.app";
     let transport = ReqwestHttpReplicaV2Transport::create(url).unwrap();
     let canister_id = Principal::from_text("aanaa-xaaaa-aaaah-aaeiq-cai").unwrap();
-    let identity =
-        Secp256k1Identity::from_pem_file(crt_path)
-            .unwrap();
+    let identity = Secp256k1Identity::from_pem_file(crt_path).unwrap();
     let agent = Agent::builder()
         .with_identity(identity)
         .with_transport(transport)
@@ -57,10 +98,10 @@ pub async fn xtc2cycle(crt_path : String, to: Principal, amount : u64) {
         .update(&canister_id, "burn")
         .with_arg(
             &Encode!(&BurnArgs {
-                    canister_id: to,
-                    amount
-                })
-                .unwrap(),
+                canister_id: to,
+                amount
+            })
+            .unwrap(),
         )
         .call_and_wait(waiter)
         .await
